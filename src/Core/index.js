b9af1512ef947ccc90c52205d36d38518de0c879
Bartek Słysz <bartekslysz@poczta.fm>
2019-04-17 15:29:17 +0200

    Imported game modules, configured them in core class.

--- src/Core/index.js
+++ src/Core/index.js
@@ -1,3 +1,10 @@
+import Map from './Map';
+import Snake from './Snake';
+import Renderer from './Renderer';
+import KeyboardManager from './KeyboardManager';
+import TouchManager from './TouchManager';
+import store from './store';
+
 const GRID_SIZE = 32;
 
 export default class Game {
@@ -7,6 +14,15 @@ export default class Game {
 
     this.canvas.width = this.width * GRID_SIZE;
     this.canvas.height = this.height * GRID_SIZE;
+
+    store.set('GAME_SIZE', [this.width, this.height]);
+  }
+
+  setDefaultStore() {
+    store.set('GAME_STATE', 'OFF');
+    store.set('GAME_SIZE', [0, 0]);
+    store.set('GAME_DIRECTION', 'RIGHT');
+    store.set('GAME_SPEED', 250);
   }
 
   constructor(canvas) {
@@ -15,6 +31,14 @@ export default class Game {
     this.canvas = canvas;
     this.context = this.canvas.getContext('2d');
 
+    this.setDefaultStore();
     this.setCanvasSize();
+
+    this.keyboardManager = new KeyboardManager();
+    this.touchManager = new TouchManager(canvas);
+
+    this.map = new Map(this.width, this.height);
+    this.snake = new Snake(this.map);
+    this.renderer = new Renderer(this.context, this.map, this.snake);
   }
 }
